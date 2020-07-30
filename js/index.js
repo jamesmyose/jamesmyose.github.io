1a2212129d87f303d7795e55ac59d445f2016d39
James Myose <jmyose@live.com>
2020-07-30 18:27:34 -0500

    failed attempt at implementing chess.js. committing to make a backup

--- js/index.js
+++ js/index.js
@@ -1,7 +1,134 @@
+// Knight Move Visualizer
+/*
 var config = {
     draggable: true,
     dropOffBoard: 'snapback',
     position: '8/8/8/8/8/8/8/1N6',
     showNotation: false,
 }
-var board1 = Chessboard('board1', config);
+var board = Chessboard('myBoard', config);
+*/
+
+// Highlight Legal Moves Example from chessboardjs.com
+// NOTE: this example uses the chess.js library:
+// https://github.com/jhlywa/chess.js
+
+var board = null
+var game = new Chess()
+var whiteSquareGrey = '#a9a9a9'
+var blackSquareGrey = '#696969'
+
+function removeGreySquares () {
+  $('#myBoard .square-55d63').css('background', '')
+}
+
+function greySquare (square) {
+  var $square = $('#myBoard .square-' + square)
+
+  var background = whiteSquareGrey
+  if ($square.hasClass('black-3c85d')) {
+    background = blackSquareGrey
+  }
+
+  $square.css('background', background)
+}
+
+function onDragStart (source, piece) {
+  // do not pick up pieces if the game is over
+  if (game.game_over()) return false
+
+  // or if it's not that side's turn
+  if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+    return false
+  }
+}
+function onDrop (source, target, piece, newPos, oldPos, orientation) {
+    removeGreySquares()
+    game.update_board(Chessboard.objToFen(newPos))
+    board.position(Chessboard.objToFen(newPos))
+    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    console.log('Source: ' + source)
+    console.log('Target: ' + target)
+    console.log('Piece: ' + piece)
+    console.log('New position: ' + Chessboard.objToFen(newPos))
+    console.log('Old position: ' + Chessboard.objToFen(oldPos))
+    console.log('Orientation: ' + orientation)
+    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    var moves = game.moves({
+      square: target,
+      verbose: true
+    })
+
+    // exit if there are no moves available for this square
+    if (moves.length === 0) {
+        console.log('no moves')
+        return
+    }
+
+    // highlight the square they moused over
+    greySquare(target)
+
+    // highlight the possible squares for this piece
+    for (var i = 0; i < moves.length; i++) {
+      greySquare(moves[i].to)
+    }
+
+
+
+}
+
+/*
+function onDrop (source, target) {
+  removeGreySquares()
+
+  // line 1750 in chess.js
+  // see if the move is legal
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q' // NOTE: always promote to a queen for example simplicity
+  })
+
+  // illegal move
+  if (move === null) return 'snapback'
+}
+*/
+
+function onMouseoverSquare (square, piece) {
+  // get list of possible moves for this square
+  var moves = game.moves({
+    square: square,
+    verbose: true
+  })
+
+  // exit if there are no moves available for this square
+  if (moves.length === 0) return
+
+  // highlight the square they moused over
+  greySquare(square)
+
+  // highlight the possible squares for this piece
+  for (var i = 0; i < moves.length; i++) {
+    greySquare(moves[i].to)
+  }
+}
+
+function onMouseoutSquare (square, piece) {
+  removeGreySquares()
+}
+
+function onSnapEnd () {
+  board.position(game.fen())
+}
+
+var config = {
+  draggable: true,
+  position: 'start',
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onMouseoutSquare: onMouseoutSquare,
+  onMouseoverSquare: onMouseoverSquare,
+  onSnapEnd: onSnapEnd
+}
+board = Chessboard('myBoard', config)
